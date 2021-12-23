ab20cfd77023abe40297619953ed02b212c8aa77
James Bradbury <me@jamesbradbury.net>
2021-12-23 00:13:25 +0000

    update aesthetic of header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,21 +1,27 @@
 <div class="header">
 	<a class='link' href='/'>Todo</a>
+    <div class="divider">|</div>
 	<a class='link' href='/done'>Done</a>
 </div>
 
 <style>
     .header {
         display: grid;
-        grid-template-columns: auto auto;
+        grid-template-columns: auto auto auto;
         gap: 1em;
         place-items: center;
         justify-content: center;
         margin-bottom: 1em;
     }
 
+    .divider {
+        font-size: 1.75rem;
+    }
+
     .link {
         font-size: 1.75rem;
         text-decoration: none;
+        color:white;
     }
 
     .link:visited {
