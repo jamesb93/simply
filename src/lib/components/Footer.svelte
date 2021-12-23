f9d25027aeabc47f7c7223455b79f2266ef85298
James Bradbury <me@jamesbradbury.net>
2021-12-23 00:13:30 +0000

    create a footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,35 @@
+<script>
+    import { state } from '$lib/app';
+    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify($state));
+
+</script>
+
 <footer>
-    
+    <a class='download' download='simply.json' href={dataStr}>Export Data</a>
+    <div class="author">
+        Made by <a target='_blank' href='https://jamesbradbury.net'>James Bradbury</a>
+    </div>
 </footer>
+
+<style>
+    footer {
+        display: flex;
+        flex-direction: row;
+        gap: 2em;
+        margin: 0 auto;
+        margin-top: auto;
+        margin-bottom: 5em;
+    }
+
+    a {
+        color: white;
+    }
+
+    a:visited {
+        color: white;
+    }
+
+    a:hover {
+        font-style: italic;
+    }
+</style>
