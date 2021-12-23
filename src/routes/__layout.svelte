298931900e9afee4fec0882eb1ceb00a91a91f99
James Bradbury <me@jamesbradbury.net>
2021-12-23 00:13:03 +0000

    add a header and footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,24 @@
 <script>
 	import '../app.css';
 	import { fade } from 'svelte/transition';
-	import Header from '$lib/components/Header.svelte'
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<Header />
+<div class="container">
+	<Header />
 
-<main>
-	<slot />
-</main>
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
